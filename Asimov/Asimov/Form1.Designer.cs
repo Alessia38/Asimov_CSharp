809a934663ac14d730d384c6d20fe439f95b364a
Pierre <pierre.Michaud3806@gmail.com>
2024-05-13 16:26:19 +0200

    CSHARP

---
+++ Asimov/Asimov/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Asimov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxUtilisateurs = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(66, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(666, 221);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBoxUtilisateurs
+            // 
+            this.comboBoxUtilisateurs.FormattingEnabled = true;
+            this.comboBoxUtilisateurs.Location = new System.Drawing.Point(66, 73);
+            this.comboBoxUtilisateurs.Name = "comboBoxUtilisateurs";
+            this.comboBoxUtilisateurs.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxUtilisateurs.TabIndex = 3;
+            this.comboBoxUtilisateurs.SelectedIndexChanged += new System.EventHandler(this.comboBoxUtilisateurs_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxUtilisateurs);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxUtilisateurs;
+    }
+}
+
---
+++ Asimov/Asimov/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Asimov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=asimov;port=3306;password=root;";
+
+            // Charger les noms et prénoms des utilisateurs avec l'ID de rôle égal à 4 dans la combobox
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT id, CONCAT(nom, ' ', prenom) AS NomComplet FROM utilisateurs WHERE idRole = 4";
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // Afficher les noms et prénoms des utilisateurs dans la combobox
+                    comboBoxUtilisateurs.DataSource = table;
+                    comboBoxUtilisateurs.DisplayMember = "NomComplet";
+                    comboBoxUtilisateurs.ValueMember = "id";
+
+                    // Empêcher l'ajout de nouvelles lignes dans la combobox
+                    comboBoxUtilisateurs.DropDownStyle = ComboBoxStyle.DropDownList;
+
+                    // Créer une colonne de boutons pour le DataGridView
+                    DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn();
+                    deleteButtonColumn.Name = "DeleteButtonColumn";
+                    deleteButtonColumn.HeaderText = "Supprimer";
+                    deleteButtonColumn.Text = "Supprimer";
+                    deleteButtonColumn.UseColumnTextForButtonValue = true;
+                    dataGridView1.Columns.Add(deleteButtonColumn);
+                }
+            }
+
+        }
+
+        private void comboBoxUtilisateurs_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            // Récupérer l'ID de l'utilisateur sélectionné dans la combobox
+            int userId = Convert.ToInt32(((DataRowView)comboBoxUtilisateurs.SelectedItem)["id"]);
+
+            // Filtrer les données de la table "moyenne" pour n'afficher que les moyennes de cet utilisateur
+            string connectionString = "server=localhost;user=root;database=asimov;port=3306;password=root;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM moyenne WHERE idUtilisateur = @userId";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@userId", userId);
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // Afficher les données filtrées dans le contrôle DataGridView
+                    dataGridView1.DataSource = table;
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Vérifier si l'utilisateur a cliqué sur le bouton de suppression
+            if (e.ColumnIndex == dataGridView1.Columns["DeleteButtonColumn"].Index && e.RowIndex >= 0)
+            {
+                // Récupérer l'ID de la ligne sélectionnée
+                int idToDelete = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["ID"].Value);
+
+                // Définir la connexion à la base de données
+                string connectionString = "server=localhost;user=root;database=asimov;port=3306;password=root;";
+
+                // Connexion à la base de données et exécution de la requête de suppression
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Définir la requête SQL de suppression
+                    string query = "DELETE FROM moyenne WHERE id = @id";
+
+                    // Créer une commande MySQL avec la requête SQL et les paramètres
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        // Ajouter le paramètre ID à la commande
+                        command.Parameters.AddWithValue("@id", idToDelete);
+
+                        // Exécuter la commande
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        // Vérifier si la suppression a réussi
+                        if (rowsAffected > 0)
+                        {
+                            // Si la suppression réussit, supprimer la ligne du DataGridView
+                            dataGridView1.Rows.RemoveAt(e.RowIndex);
+                        }
+                        else
+                        {
+                            // Si la suppression échoue, afficher un message d'erreur ou effectuer d'autres actions nécessaires
+                            MessageBox.Show("La suppression a échoué.");
+                        }
+                    }
+                }
+
+                // Supprimer la ligne du DataGridView
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
+        }
+
+    }
+}
